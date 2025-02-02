b71f947e6a529fdd035e338aed1fbe21bdf886ba
gxres042 <i@gxres.net>
2025-02-02 11:44:44 +0800

    init

---
+++ components/PostList.server.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script lang="ts" setup>
+import { initNoutious } from "~~/noutious";
+const noutious = await initNoutious();
+const posts = noutious.getPosts();
+console.log(posts)
+</script>
+
+<style></style>
---
+++ posts/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: Hello World
+date: 2025-01-21
+---
+
+Hello!
